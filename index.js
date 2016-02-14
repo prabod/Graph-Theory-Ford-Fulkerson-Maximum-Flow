53df2d8de20298953f217f72650cc81e9cb5f0f8
prabod <prabod@rathnayaka.me>
2016-02-15 00:22:43 +0530

    First

---
+++ index.js
@@ -0,0 +1,70 @@
+'use strict'
+
+module.exports = fordFulkerson
+
+function bfs(rGraph, s, t, parent) {
+    var visited = [];
+    var queue = [];
+    var V = rGraph.length;
+    // Create a visited array and mark all vertices as not visited
+    for (var i = 0; i < V; i++) {
+        visited[i] = false;
+    }
+    // Create a queue, enqueue source vertex and mark source vertex as visited
+    queue.push(s);
+    visited[s] = true;
+    parent[s] = -1;
+
+    while (queue.length != 0) {
+        var u = queue.shift();
+        for (var v = 0; v < V; v++) {
+            if (visited[v] == false && rGraph[u][v] > 0) {
+                queue.push(v);
+                parent[v] = u;
+                visited[v] = true;
+            }
+        }
+    }
+    //If we reached sink in BFS starting from source, then return true, else false
+    return (visited[t] == true);
+}
+
+function fordFulkerson(graph, s, t) {
+      /* Create a residual graph and fill the residual graph
+       with given capacities in the original graph as
+       residual capacities in residual graph
+
+       Residual graph where rGraph[i][j] indicates
+       residual capacity of edge from i to j (if there
+       is an edge. If rGraph[i][j] is 0, then there is
+       not)
+      */
+    var rGraph = [];
+    for (var u = 0; u < graph.length; u++) {
+        var temp = [];
+        for (v = 0; v < graph.length; v++) {
+            temp.push(u * v);
+        }
+        rGraph.push(temp);
+    }
+    var parent = [];
+    var maxFlow = 0;
+
+    while (bfs(rGraph, s, t, parent)) {
+        var pathFlow = Number.MAX_VALUE;
+        for (var v = t; v != s; v = parent[v]) {
+            u = parent[v];
+            pathFlow = Math.min(pathFlow, rGraph[u][v]);
+        }
+        for (v = t; v != s; v = parent[v]) {
+            u = parent[v];
+            rGraph[u][v] -= pathFlow;
+            rGraph[v][u] += pathFlow;
+        }
+
+
+        maxFlow += pathFlow;
+    }
+    // Return the overall flow
+    return maxFlow;
+}
---
+++ test/tests.js
@@ -0,0 +1,22 @@
+/**
+ * Created by prabod on 2/14/16.
+ */
+'use strict'
+var fordFulkerson = require('../index');
+var graph = [
+	[
+		0, 16, 13, 0, 0, 0
+	], [
+		0, 0, 10, 12, 0, 0
+	], [
+		0, 4, 0, 0, 14, 0
+	], [
+		0, 0, 9, 0, 0, 20
+	], [
+		0, 0, 0, 7, 0, 4
+	], [
+		0, 0, 0, 0, 0, 0
+	]
+];
+alert("The maximum possible flow is " +
+	fordFulkerson(graph, 0, 5));
