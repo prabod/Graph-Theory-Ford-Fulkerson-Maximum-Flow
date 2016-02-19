b2dbd3b6880b76f9ca4f7d76f5232327499b9e79
Prabod Rathnayaka <prabod@rathnayaka.me>
2016-02-19 16:48:25 +0530

    Updated tests

--- test/index.js
+++ test/index.js
@@ -19,3 +19,39 @@ test('a basic example', function(t) {
 
   t.end()
 })
+
+test('for invalid source or sink', function(t) {
+  var graph = [
+    [ 0, 16, 13,  0,  0,  0 ],
+    [ 0,  0, 10, 12,  0,  0 ],
+    [ 0,  4,  0,  0, 14,  0 ],
+    [ 0,  0,  9,  0,  0, 20 ],
+    [ 0,  0,  0,  7,  0,  4 ],
+    [ 0,  0,  0,  0,  0,  0 ]
+  ];
+
+  t.throws(fordFulkerson(graph, 0, 6));
+  t.throws(fordFulkerson(graph, -1, 5));
+  t.throws(fordFulkerson(graph, 0, -1));
+  t.end()
+})
+test('for invalid graph which is not size of NxN ', function(t) {
+  var graph = [
+    [ 0, 16, 13,  0,  0,  0 ],
+    [ 0,  0, 10, 12],
+    [ 0,  4,  0,  0, 14,  0 ],
+    [ 0,  0,  9,  0,  0, 20 ],
+    [ 0,  0,  0,  7,  0,  4 ],
+    [ 0,  0,  0,  0,  0,  0 ]
+  ];
+
+  t.throws(fordFulkerson(graph, 0, 5));
+  t.end();
+})
+
+test('for invalid graph which is not size of NxN ', function(t) {
+  var graph = [];
+
+  t.throws(fordFulkerson(graph, 0, 5));
+  t.end();
+})
